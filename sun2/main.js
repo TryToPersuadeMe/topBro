610d29d5e29bb6c4d3006f01d0188b4ceea3b5fc
Dmitry <zombak1234@yandex.ru>
2021-03-22 02:55:16 +0300

    1

---
+++ sun2/main.js
@@ -0,0 +1,106 @@
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+
+import { GUI } from "https://unpkg.com/three@0.126.1/examples/jsm/libs/dat.gui.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.124.0/examples/jsm/controls/OrbitControls.js";
+import { Sky } from "https://unpkg.com/three@0.126.1/examples/jsm/objects/Sky.js";
+
+let camera, scene, renderer;
+
+let sky, sun;
+
+init();
+render();
+
+function initSky() {
+  // Add Sky
+  sky = new Sky();
+  sky.scale.setScalar(450000);
+  scene.add(sky);
+
+  sun = new THREE.Vector4();
+  /// GUI
+
+  const effectController = {
+    turbidity: 10,
+    rayleigh: 3,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.7,
+    inclination: 0.49, // elevation / inclination
+    azimuth: 0.25, // Facing front,
+    exposure: renderer.toneMappingExposure,
+  };
+
+  function guiChanged() {
+    const uniforms = sky.material.uniforms;
+    uniforms["turbidity"].value = effectController.turbidity;
+    uniforms["rayleigh"].value = effectController.rayleigh;
+    uniforms["mieCoefficient"].value = effectController.mieCoefficient;
+    uniforms["mieDirectionalG"].value = effectController.mieDirectionalG;
+
+    const theta = Math.PI * (effectController.inclination - 0.5);
+    const phi = 2 * Math.PI * (effectController.azimuth - 0.5);
+
+    sun.x = Math.cos(phi);
+    sun.y = Math.sin(phi) * Math.sin(theta);
+    sun.z = Math.sin(phi) * Math.cos(theta);
+
+    uniforms["sunPosition"].value.copy(sun);
+
+    renderer.toneMappingExposure = effectController.exposure;
+    renderer.render(scene, camera);
+  }
+
+  const gui = new GUI();
+
+  gui.add(effectController, "turbidity", 0.0, 20.0, 0.1).onChange(guiChanged);
+  gui.add(effectController, "rayleigh", 0.0, 4, 0.001).onChange(guiChanged);
+  gui.add(effectController, "mieCoefficient", 0.0, 0.1, 0.001).onChange(guiChanged);
+  gui.add(effectController, "mieDirectionalG", 0.0, 1, 0.001).onChange(guiChanged);
+  gui.add(effectController, "inclination", 0, 1, 0.0001).onChange(guiChanged);
+  gui.add(effectController, "azimuth", 0, 1, 0.0001).onChange(guiChanged);
+  gui.add(effectController, "exposure", 0, 1, 0.0001).onChange(guiChanged);
+
+  guiChanged();
+}
+
+function init() {
+  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 200000);
+  camera.position.set(0, 100, 3000);
+
+  scene = new THREE.Scene();
+
+  const helper = new THREE.GridHelper(10000, 25, 0x95f2f2, 0x95f2f2);
+  scene.add(helper);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 0.04;
+  document.body.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener("change", render);
+  //   controls.maxPolarAngle = Math.PI / 2;
+  controls.enableZoom = false;
+  controls.enablePan = false;
+
+  initSky();
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+/* https://customizer.github.io/three.js-doc.ru/three.js-ru.htm */
