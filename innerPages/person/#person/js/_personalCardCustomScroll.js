5d96fc50d35642077a2dc01238bbf383a4c2ae7c
TryToPersuadeMe <61381766+TryToPersuadeMe@users.noreply.github.com>
2021-03-24 17:38:33 +0300

    final

---
+++ innerPages/person/#person/js/_personalCardCustomScroll.js
@@ -0,0 +1,11 @@
+var personalCardCustomScroll = new Swiper(".personalCard__containerInfo", {
+  direction: "vertical",
+  slidesPerView: "auto",
+  freeMode: true,
+  freeModeMomentum: false,
+  freeModeMomentumBounce: false,
+  scrollbar: {
+    el: ".personalCard__scrollBar",
+  },
+  mousewheel: true,
+});
